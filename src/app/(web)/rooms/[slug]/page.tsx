196f4df7d5fffbf9d3ab46db2fbd7f1194019504
Aliaksandr Bandziuk <62721784+aliaksandr-bandziuk@users.noreply.github.com>
2024-02-25 20:58:50 +0100

    full project

--- src/app/(web)/rooms/[slug]/page.tsx
+++ src/app/(web)/rooms/[slug]/page.tsx
@@ -1,35 +1,39 @@
 'use client';
 
-import { getRoom } from "@/libs/apis";
-import useSWR from "swr";
-import LoadingSpinner from "../../loading";
-import HotelPhotoGallery from "@/components/HotelPhotoGallery/HotelPhotoGallery";
-import { MdOutlineCleaningServices } from "react-icons/md";
-import { LiaFireExtinguisherSolid } from "react-icons/lia";
-import { AiOutlineMedicineBox } from "react-icons/ai";
-import { GiSmokeBomb } from "react-icons/gi";
-import BookRoomCta from "@/components/BookRoomCta/BookRoomCta";
-import { useState } from "react";
-import toast from "react-hot-toast";
-import axios from "axios";
-import { getStripe } from "@/libs/stripe";
+import useSWR from 'swr';
+import { MdOutlineCleaningServices } from 'react-icons/md';
+import { LiaFireExtinguisherSolid } from 'react-icons/lia';
+import { AiOutlineMedicineBox } from 'react-icons/ai';
+import { GiSmokeBomb } from 'react-icons/gi';
+import { useState } from 'react';
+import axios from 'axios';
+
+import { getRoom } from '@/libs/apis';
+import LoadingSpinner from '../../loading';
+import HotelPhotoGallery from '@/components/HotelPhotoGallery/HotelPhotoGallery';
+import BookRoomCta from '@/components/BookRoomCta/BookRoomCta';
+import toast from 'react-hot-toast';
+import { getStripe } from '@/libs/stripe';
+import RoomReview from '@/components/RoomReview/RoomReview';
 
 const RoomDetails = (props: { params: { slug: string } }) => {
-
-  const { params: { slug } } = props;
+  const {
+    params: { slug },
+  } = props;
 
   const [checkinDate, setCheckinDate] = useState<Date | null>(null);
   const [checkoutDate, setCheckoutDate] = useState<Date | null>(null);
-  const [adults, setAdults] = useState<number>(1);
-  const [children, setChildren] = useState<number>(0);
+  const [adults, setAdults] = useState(1);
+  const [children, setChildren] = useState(0);
 
   const fetchRoom = async () => getRoom(slug);
 
-  const { data: room, error, isLoading } = useSWR("/api/room", fetchRoom);
+  const { data: room, error, isLoading } = useSWR('/api/room', fetchRoom);
 
-  if (error) throw new Error("Cannot fetch data from API");
-  if (typeof room === "undefined" && !isLoading)
-    throw new Error("Cannot fetch data from API");
+  if (error) throw new Error('Cannot fetch data');
+  if (typeof room === 'undefined' && !isLoading)
+    throw new Error('Cannot fetch data');
+
   if (!room) return <LoadingSpinner />;
 
   const calcMinCheckoutDate = () => {
@@ -42,17 +46,16 @@ const RoomDetails = (props: { params: { slug: string } }) => {
   };
 
   const handleBookNowClick = async () => {
+    if (!checkinDate || !checkoutDate)
+      return toast.error('Please provide checkin / checkout date');
 
-    if (!checkinDate || !checkoutDate) {
-      return toast.error("Please provide check-in / check-out dates");
-    }
-    if (checkinDate > checkoutDate) {
-      return toast.error("Check-out date must be greater than check-in date");
-    }
-    const numberOfDays = calcNoOfDays();
+    if (checkinDate > checkoutDate)
+      return toast.error('Please choose a valid checkin period');
+
+    const numberOfDays = calcNumDays();
+
     const hotelRoomSlug = room.slug.current;
 
-    // Integrate Stripe payment here
     const stripe = await getStripe();
 
     try {
@@ -62,7 +65,7 @@ const RoomDetails = (props: { params: { slug: string } }) => {
         adults,
         children,
         numberOfDays,
-        hotelRoomSlug
+        hotelRoomSlug,
       });
 
       if (stripe) {
@@ -71,110 +74,104 @@ const RoomDetails = (props: { params: { slug: string } }) => {
         });
 
         if (result.error) {
-          toast.error("Payment failed");
+          toast.error('Payment Failed');
         }
       }
-
     } catch (error) {
-      console.log("Error:", error);
-      toast.error("An error occurred while processing your payment. Please try again later.");
+      console.log('Error: ', error);
+      toast.error('An error occured');
     }
-
   };
 
-  const calcNoOfDays = () => {
-    if (!checkinDate || !checkoutDate) return 0;
+  const calcNumDays = () => {
+    if (!checkinDate || !checkoutDate) return;
     const timeDiff = checkoutDate.getTime() - checkinDate.getTime();
     const noOfDays = Math.ceil(timeDiff / (24 * 60 * 60 * 1000));
     return noOfDays;
   };
 
-  // console.log(room);
-
   return (
     <div>
       <HotelPhotoGallery photos={room.images} />
-      <div className="container mx-auto mt-20">
-        <div className="md:grid md:grid-cols-12 gap-10 px-3">
-          <div className="md:col-span-8 md:w-full">
-            {/* HOTEL INFORMATION */}
+
+      <div className='container mx-auto mt-20'>
+        <div className='md:grid md:grid-cols-12 gap-10 px-3'>
+          <div className='md:col-span-8 md:w-full'>
             <div>
-              <h2 className="font-bold text-left text-lg md:text-2xl">
+              <h2 className='font-bold text-left text-lg md:text-2xl'>
                 {room.name} ({room.dimension})
               </h2>
-              <div className="flex my-11">
-                {room.offeredAmenities.map((amenity) => (
+              <div className='flex my-11'>
+                {room.offeredAmenities.map(amenity => (
                   <div
                     key={amenity._key}
-                    className="md:w-44 w-fit text-center px-2 md:px-0 h-20 md:h-40 mr-3 bg-[#eff0f2] dark:bg-gray-800 rounded-lg grid place-content-center"
+                    className='md:w-44 w-fit text-center px-2 md:px-0 h-20 md:h-40 mr-3 bg-[#eff0f2] dark:bg-gray-800 rounded-lg grid place-content-center'
                   >
                     <i className={`fa-solid ${amenity.icon} md:text-2xl`}></i>
-                    <p className="text-xs md:text-base pt-3">{amenity.amenity}</p>
+                    <p className='text-xs md:text-base pt-3'>
+                      {amenity.amenity}
+                    </p>
                   </div>
                 ))}
               </div>
-              <div className="mb-11">
-                <h2 className="font-bold text-3xl mb-2">Description</h2>
+              <div className='mb-11'>
+                <h2 className='font-bold text-3xl mb-2'>Description</h2>
                 <p>{room.description}</p>
               </div>
-              <div className="mb-11">
-                <h2 className="font-bold text-3xl mb-2">Offered Amenities</h2>
-                <div className="grid grid-cols-2">
-                  {room.offeredAmenities.map((amenity) => (
+              <div className='mb-11'>
+                <h2 className='font-bold text-3xl mb-2'>Offered Amenities</h2>
+                <div className='grid grid-cols-2'>
+                  {room.offeredAmenities.map(amenity => (
                     <div
                       key={amenity._key}
-                      className="flex items-center md:my-0 my-1"
+                      className='flex items-center md:my-0 my-1'
                     >
                       <i className={`fa-solid ${amenity.icon}`}></i>
-                      <p className="text-xs md:text-base ml-2">{amenity.amenity}</p>
+                      <p className='text-xs md:text-base ml-2'>
+                        {amenity.amenity}
+                      </p>
                     </div>
                   ))}
                 </div>
               </div>
-              <div className="mb-11">
-                <h2 className="font-bold text-3xl mb-2">Safety and Hygiene</h2>
-                <div className="grid grid-cols-2">
-                  <div className="flex items-center my-1 md:my-0">
+              <div className='mb-11'>
+                <h2 className='font-bold text-3xl mb-2'>Safety And Hygiene</h2>
+                <div className='grid grid-cols-2'>
+                  <div className='flex items-center my-1 md:my-0'>
                     <MdOutlineCleaningServices />
-                    <p className="ml-2 md:text-base text-xs">
-                      Daily Cleaning
-                    </p>
+                    <p className='ml-2 md:text-base text-xs'>Daily Cleaning</p>
                   </div>
-                  <div className="flex items-center my-1 md:my-0">
+                  <div className='flex items-center my-1 md:my-0'>
                     <LiaFireExtinguisherSolid />
-                    <p className="ml-2 md:text-base text-xs">
+                    <p className='ml-2 md:text-base text-xs'>
                       Fire Extinguishers
                     </p>
                   </div>
-                  <div className="flex items-center my-1 md:my-0">
+                  <div className='flex items-center my-1 md:my-0'>
                     <AiOutlineMedicineBox />
-                    <p className="ml-2 md:text-base text-xs">
-                      Sterilizations
+                    <p className='ml-2 md:text-base text-xs'>
+                      Disinfections and Sterilizations
                     </p>
                   </div>
-                  <div className="flex items-center my-1 md:my-0">
+                  <div className='flex items-center my-1 md:my-0'>
                     <GiSmokeBomb />
-                    <p className="ml-2 md:text-base text-xs">
-                      Smoke Detectors
-                    </p>
+                    <p className='ml-2 md:text-base text-xs'>Smoke Detectors</p>
                   </div>
                 </div>
               </div>
 
-              <div className="shadow dark:shadow-white rounded-lg p-6">
-                <div className="items-center mb-4">
-                  <p className="md:text-lg font-semibold">Customer Reviews</p>
+              <div className='shadow dark:shadow-white rounded-lg p-6'>
+                <div className='items-center mb-4'>
+                  <p className='md:text-lg font-semibold'>Customer Reviews</p>
                 </div>
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                  {/* Reviews */}
-
+                <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+                  <RoomReview roomId={room._id} />
                 </div>
               </div>
             </div>
           </div>
 
-          <div className="md:col-span-4 rounded-xl shadow-lg dark:shadow dark:shadow-white sticky top-10 h-fit overflow-auto">
-            {/* BOOK ROOM CTA */}
+          <div className='md:col-span-4 rounded-xl shadow-lg dark:shadow dark:shadow-white sticky top-10 h-fit overflow-auto'>
             <BookRoomCta
               discount={room.discount}
               price={room.price}
@@ -185,8 +182,8 @@ const RoomDetails = (props: { params: { slug: string } }) => {
               setCheckoutDate={setCheckoutDate}
               calcMinCheckoutDate={calcMinCheckoutDate}
               adults={adults}
-              setAdults={setAdults}
               children={children}
+              setAdults={setAdults}
               setChildren={setChildren}
               isBooked={room.isBooked}
               handleBookNowClick={handleBookNowClick}
@@ -195,7 +192,7 @@ const RoomDetails = (props: { params: { slug: string } }) => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default RoomDetails
+export default RoomDetails;
